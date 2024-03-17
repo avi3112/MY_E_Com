1db14fdaa63e93c5161e40ced07ab1c030389cb2
Avinabh Kumar <kumar.avi3112@gmail.com>
2024-03-17 16:33:01 +0530

    created a header and footer on this UI side to look good

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
-    <div className="bg-dark text-light p-3">
+    <div className="footer">
       <h1 className="text-center">All Right Copy @Techbazar</h1>
+      <p className="text-center">
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/privacy">Privacy</Link>
+      </p>
     </div>
   );
 };
--- client/src/pages/PagenotFound.js
+++ client/src/pages/PagenotFound.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 
 export const PagenotFound = () => {
   return (
     <Layout>
-      <div>
-        <h1>PagenotFound</h1>
+      <div className="container">
+        <h1 className="centered-div">404</h1>
+        <h2 className="pnf-heading">!ooopsss ! Page notFound</h2>
+        <Link to="/" className="pnf-btn">
+          Go Back
+        </Link>
       </div>
     </Layout>
   );
