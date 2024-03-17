c2e0de07b9bee36eca9609c5b0341edf19d3ec2e
Avinabh Kumar <kumar.avi3112@gmail.com>
2024-03-17 10:59:27 +0530

    user registration api done

--- controller/authController.js
+++ controller/authController.js
@@ -1,1 +1,47 @@
-export const registerController = () => {};
+import { hashpassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
+
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+    //validations
+    if (!name && !email && !password && !phone && !address) {
+      return res.send({ error: `${name} is required` });
+    }
+    // check user
+    const existinguser = await userModel.findOne({ email });
+
+    // existing user
+    if (existinguser) {
+      return res.status(200).send({
+        success: true,
+        message: "Already register please login",
+      });
+    }
+    // register user
+
+    const hashedPassword = await hashpassword(password);
+
+    // save
+
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "user register successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registeration",
+      error,
+    });
+  }
+};
--- models/userModel.js
+++ models/userModel.js
@@ -32,4 +32,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose("users", userSchema);
+export default mongoose.model("users", userSchema);
