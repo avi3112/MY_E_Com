3bc22d99ca0644f62eb2a4e0cbf7a3d603c007a6
Avinabh Kumar <kumar.avi3112@gmail.com>
2024-03-17 14:07:50 +0530

    created middleware for signin with token and admil route middleware

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,10 @@ import express from "express";
 import {
   registerController,
   loginController,
+  testController,
 } from "../controller/authController.js";
+import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
+
 // router object
 const router = express.Router();
 
@@ -11,5 +14,5 @@ const router = express.Router();
 
 router.post("/register", registerController);
 router.post("/login", loginController);
-
+router.get("/test", requireSignIn, isAdmin, testController);
 export default router;
